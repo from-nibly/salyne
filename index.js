bacf6cfadc02951542f4fd9e160a1cf12554580f
jordan <jordan.davidson@domo.com>
2016-02-01 09:56:23 -0700

    Fixed bugs

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 var fs = require('fs'),
   path = require('path');
-exports = module.exports = function Salyne() {
+exports = module.exports = function Salyne(options) {
   var registry = {};
-  var parent = module;
-  while(parent.parent) {
-    parent = parent.parent;
+  var parent;
+  options = options || {};
+  if (options.parent) {
+    parent = options.parent;
+  } else {
+    parent = module;
+    while (parent.parent) {
+      parent = parent.parent;
+    }
   }
 
   this.create = function(name, dir) {
     dir = dir || [];
     var entry = registry[name];
-    if(!entry) {
+    if (!entry) {
       try {
         return parent.require(name);
-      } catch(e) {
+      } catch (e) {
         throw new Error(`could not find dependency ${name}`);
       }
-    } else if(entry.options.singleton === true && entry.instance) {
+    } else if (entry.options.singleton === true && entry.instance) {
       return entry.instance;
     } else {
       var instance;
       var args = [];
-      for(var req of entry.requires) {
+      for (var req of entry.requires) {
         var newDir = dir.slice();
         newDir.push(req);
-        if(newDir.indexOf(req) !== newDir.length - 1) {
+        if (newDir.indexOf(req) !== newDir.length - 1) {
           throw new Error(`circular dependency detected: ${newDir.toString()}`);
         }
         args.push(this.create(req, newDir));
@@ -39,7 +45,8 @@ exports = module.exports = function Salyne() {
     var name;
     var options;
     //setup incoming arguments
-    args = Object.keys(arguments).map(x => arguments[x]);
+    args = Object.keys(arguments)
+      .map(x => arguments[x]);
     for (var arg of args) {
       if (typeof arg === 'function') {
         func = arg;
@@ -66,16 +73,17 @@ exports = module.exports = function Salyne() {
     };
 
     //get dependencies
-    if(func.requires || func.require) {
+    if (func.requires || func.require) {
       entry.requires = func.requires || func.require;
-    } else if(func['@requires'] || func['@require']) {
+    } else if (func['@requires'] || func['@require']) {
       entry.requires = func['@requires'] || func['@require'];
     } else {
       entry.requires = [];
-      var argString = func.toString().match(/function\s*[^\(]*\(\s*([^\)]*)\)/m)[1];
+      var argString = func.toString()
+        .match(/function\s*[^\(]*\(\s*([^\)]*)\)/m)[1];
       var argStrings = argString.split(',');
-      for(arg of argStrings) {
-        if(arg.match(/^\s*$/)) {
+      for (arg of argStrings) {
+        if (arg.match(/^\s*$/)) {
           continue;
         }
         arg = arg.replace(/\s/, '');
@@ -91,35 +99,36 @@ exports = module.exports = function Salyne() {
     var name;
     var fileName;
     var options;
-    args = Object.keys(arguments).map(x => arguments[x]);
+    args = Object.keys(arguments)
+      .map(x => arguments[x]);
     for (var arg of args) {
-      if(typeof arg === 'string') {
-        if(arg.endsWith('.js') || arg.endsWith('.json') || arg.indexOf('/') !== -1 || arg.indexOf('\\') !== -1) {
+      if (typeof arg === 'string') {
+        if (arg.endsWith('.js') || arg.endsWith('.json') || arg.indexOf('/') !== -1 || arg.indexOf('\\') !== -1) {
           fileName = path.join(path.dirname(parent.filename), arg);
         } else {
           name = arg;
         }
-      } else if(typeof arg === 'object') {
+      } else if (typeof arg === 'object') {
         options = arg;
       }
     }
     //initialize fileNames
     var fileNames = [];
-    if(!fileName.endsWith('.js') && !fileName.endsWith('.json')) {
-      for(var file of fs.readdirSync(fileName)) {
+    if (!fileName.endsWith('.js') && !fileName.endsWith('.json')) {
+      for (var file of fs.readdirSync(fileName)) {
         fileNames.push(path.join(fileName, file));
       }
     } else {
       fileNames.push(fileName);
     }
     //setup name
-    for(var file of fileNames) {
-      if(!file.endsWith('.js') && !file.endsWith('.json')) {
+    for (var file of fileNames) {
+      if (!file.endsWith('.js') && !file.endsWith('.json')) {
         continue;
       }
       var ctor = parent.require(file);
       var nameReg = /\/?([\w\.]*)(.js|.json)/gmi
-      if(fileNames.length === 1) {
+      if (fileNames.length === 1) {
         name = name || ctor.name || nameReg.exec(file)[1];
       } else {
         name = ctor.name || nameReg.exec(file)[1];
