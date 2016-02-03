48986adb9c5fe2edba9d3c460fcece1c1e09b0b7
from-nibly <thatoneemail@gmail.com>
2016-02-02 21:25:54 -0700

    Fixed license

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2016 Jordan S. Davidson
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
 var fs = require('fs'),
   path = require('path');
 exports = module.exports = function Salyne(options) {
@@ -11,6 +14,7 @@ exports = module.exports = function Salyne(options) {
     while (parent.parent) {
       parent = parent.parent;
     }
+
   }
 
   this.create = function(name, dir) {
