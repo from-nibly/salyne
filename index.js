ffc2512beefc953fa7c847a87214d28888277ebf
Jordan Davidson <jordan.davidson@domo.com>
2016-06-22 16:47:50 -0600

    fixed bug with bind function

--- index.js
+++ index.js
@@ -147,6 +147,7 @@ exports = module.exports = function Salyne(options) {
     var func = util.getFuncArg(arguments);
     var name = util.getStringArg(arguments);
     var options = util.getObjectArg(arguments) || {};
+    var requires = util.getArrayArg(arguments);
 
     //initialize blank arguments
     if (!func) {
@@ -164,7 +165,7 @@ exports = module.exports = function Salyne(options) {
     };
 
     //get dependencies
-    entry.requires = extractArgs(func);
+    entry.requires = requires ||  extractArgs(func);
 
     //put values in registry
     registry[name] = entry;
